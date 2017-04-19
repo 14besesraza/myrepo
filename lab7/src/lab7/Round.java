16fd24f0421df52dce1cb2853cc10343bf852a38
14besesraza <14besesraza@seecs.edu.pk>
2017-04-19 14:10:59 +0500

    Add files via upload

---
+++ lab7/src/lab7/Round.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7;
+
+/**
+ *
+ * @author sraza.bese14seecs
+ */
+public class Round {
+    @id
+    private String userid;
+    @column
+    private boolean state1;
+    
+    @column
+    private String scheme;
+    
+    @column
+    private double time_taken;
+    
+    @column
+    private double T_c1;
+    
+    @column
+    private boolean state2;
+    
+    @column
+    private double T_c2;
+    
+    @column
+    private boolean state3;
+    
+    @column
+    private double t_c3;
+    
+    @column
+    private boolean state4;
+    
+    @column
+    private double t_c4;
+    
+    @column
+    private boolean state5;
+    
+    @column
+    private double t_c5;
+    
+    @column
+    private boolean state6;
+    
+    @column
+    private double t_c6;
+    
+    @column
+    private boolean state7;
+    
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public boolean isState1() {
+        return state1;
+    }
+
+    public void setState1(boolean state1) {
+        this.state1 = state1;
+    }
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public void setScheme(String scheme) {
+        this.scheme = scheme;
+    }
+
+    public double getTime_taken() {
+        return time_taken;
+    }
+
+    public void setTime_taken(double time_taken) {
+        this.time_taken = time_taken;
+    }
+
+    public double getT_c1() {
+        return T_c1;
+    }
+
+    public void setT_c1(double T_c1) {
+        this.T_c1 = T_c1;
+    }
+
+    public boolean isState2() {
+        return state2;
+    }
+
+    public void setState2(boolean state2) {
+        this.state2 = state2;
+    }
+
+    public double getT_c2() {
+        return T_c2;
+    }
+
+    public void setT_c2(double T_c2) {
+        this.T_c2 = T_c2;
+    }
+
+    public boolean isState3() {
+        return state3;
+    }
+
+    public void setState3(boolean state3) {
+        this.state3 = state3;
+    }
+
+    public double getT_c3() {
+        return t_c3;
+    }
+
+    public void setT_c3(double t_c3) {
+        this.t_c3 = t_c3;
+    }
+
+    public boolean isState4() {
+        return state4;
+    }
+
+    public void setState4(boolean state4) {
+        this.state4 = state4;
+    }
+
+    public double getT_c4() {
+        return t_c4;
+    }
+
+    public void setT_c4(double t_c4) {
+        this.t_c4 = t_c4;
+    }
+
+    public boolean isState5() {
+        return state5;
+    }
+
+    public void setState5(boolean state5) {
+        this.state5 = state5;
+    }
+
+    public double getT_c5() {
+        return t_c5;
+    }
+
+    public void setT_c5(double t_c5) {
+        this.t_c5 = t_c5;
+    }
+
+    public boolean isState6() {
+        return state6;
+    }
+
+    public void setState6(boolean state6) {
+        this.state6 = state6;
+    }
+
+    public double getT_c6() {
+        return t_c6;
+    }
+
+    public void setT_c6(double t_c6) {
+        this.t_c6 = t_c6;
+    }
+
+    public boolean isState7() {
+        return state7;
+    }
+
+    public void setState7(boolean state7) {
+        this.state7 = state7;
+    }
+    
+   
+    
+    
+}
